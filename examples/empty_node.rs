5a4b95f9a987c19d04df07f0d766f39d196a71df
boshtannik <boshtannik@gmail.com>
2024-01-20 13:35:00 +0200

    initial

---
+++ examples/empty_node.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+#![feature(abi_avr_interrupt)]
+
+use arduino_hal::default_serial;
+use embedded_nano_mesh::{AddressType, Node, NodeConfig};
+use panic_halt as _;
+
+use platform_millis_arduino_nano::{init_timer, ms, Atmega328pTime};
+use platform_serial_arduino_nano::{init_serial, ArduinoNanoSerial};
+
+#[arduino_hal::entry]
+fn main() -> ! {
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+
+    init_timer(dp.TC0);
+    init_serial(default_serial!(dp, pins, 9600));
+
+    let mut mesh_node = Node::new(NodeConfig {
+        device_address: AddressType::MAX,
+        listen_period: 100 as ms,
+    });
+
+    loop {
+        let _ = mesh_node.update::<Atmega328pTime, ArduinoNanoSerial>();
+    }
+}
